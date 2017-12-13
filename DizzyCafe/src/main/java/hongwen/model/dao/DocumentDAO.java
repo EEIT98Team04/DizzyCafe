f53a51575cd4c2bd10f50fa21c2dbdb4dda3b87a
黃鴻文 <longmengua@gmail.com>
2017-12-13 11:48:33 +0800

    20171213
    merge

---
+++ DizzyCafe/src/main/java/hongwen/model/dao/DocumentDAO.java
@@ -0,0 +1,19 @@
+package hongwen.model.dao;
+
+import java.util.List;
+
+import hongwen.model.bean.*;
+
+public interface DocumentDAO {
+
+	ProductBean select(int id);
+
+	List<ProductBean> select();
+
+	ProductBean insert(ProductBean bean);
+
+	ProductBean update(String name, double price, java.util.Date make, int expire, int id);
+
+	boolean delete(int id);
+
+}

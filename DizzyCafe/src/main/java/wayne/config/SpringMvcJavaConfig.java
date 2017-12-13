9b457e9e5d1f3f9beca16a46eb47d5ba307b03f0
于謹憲 <ppp@eeit>
2017-12-13 16:56:35 +0800

    第一次commit

---
+++ DizzyCafe/src/main/java/wayne/config/SpringMvcJavaConfig.java
@@ -0,0 +1,13 @@
+package wayne.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(basePackages="wayne.controller")
+@EnableWebMvc
+public class SpringMvcJavaConfig implements WebMvcConfigurer {
+
+}

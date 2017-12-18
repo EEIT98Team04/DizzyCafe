dc68bd5108d524860f61673cfa5b94aca6332136
簡明輝 <todoenil@gmail.com>
2017-12-18 15:50:11 +0800

    commit
    member修改欄位memberPlay int -> java.sql.Date
    新增LoginFilter, SecurityFilter

--- DizzyCafe/src/main/java/minghui/model/MemberBean.java
+++ DizzyCafe/src/main/java/minghui/model/MemberBean.java
@@ -6,14 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
 @Entity
 @Table(name = "MEMBER")
 public class MemberBean {
@@ -27,7 +19,7 @@ public class MemberBean {
 	private java.util.Date memberBirth;
 	private String memberAddress;
 	private int memberStatus; // 狀態預留欄位(0 : 黑名單 1 : 正常狀態)(留言板)
-	private int memberPlay; // 每日活動狀態碼
+	private java.sql.Date memberPlay; // 每日活動狀態碼
 	private String memberPhoto;
 
 	@Override
@@ -102,11 +94,11 @@ public class MemberBean {
 		this.memberStatus = memberStatus;
 	}
 
-	public int getMemberPlay() {
+	public java.sql.Date getMemberPlay() {
 		return memberPlay;
 	}
 
-	public void setMemberPlay(int memberPlay) {
+	public void setMemberPlay(java.sql.Date memberPlay) {
 		this.memberPlay = memberPlay;
 	}
 
